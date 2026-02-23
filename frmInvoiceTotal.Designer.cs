6a5867a81206722910297cdefd5731a5b320377f
jrich <jrich3@smartweb.augustatech.edu>
2026-02-23 14:50:39 -0500

    homework

---
+++ frmInvoiceTotal.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Homework2
+{
+    partial class frmInvoiceTotal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtSubtotal = new TextBox();
+            txtDiscountPercent = new TextBox();
+            txtDiscountAmount = new TextBox();
+            txtTotal = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btnCalculate = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // txtSubtotal
+            // 
+            txtSubtotal.Location = new Point(187, 52);
+            txtSubtotal.Name = "txtSubtotal";
+            txtSubtotal.Size = new Size(138, 27);
+            txtSubtotal.TabIndex = 1;
+            txtSubtotal.TextChanged += textBox1_TextChanged;
+            // 
+            // txtDiscountPercent
+            // 
+            txtDiscountPercent.Location = new Point(187, 106);
+            txtDiscountPercent.Name = "txtDiscountPercent";
+            txtDiscountPercent.ReadOnly = true;
+            txtDiscountPercent.Size = new Size(138, 27);
+            txtDiscountPercent.TabIndex = 1;
+            txtDiscountPercent.TabStop = false;
+            // 
+            // txtDiscountAmount
+            // 
+            txtDiscountAmount.Location = new Point(187, 160);
+            txtDiscountAmount.Name = "txtDiscountAmount";
+            txtDiscountAmount.ReadOnly = true;
+            txtDiscountAmount.Size = new Size(138, 27);
+            txtDiscountAmount.TabIndex = 2;
+            txtDiscountAmount.TabStop = false;
+            // 
+            // txtTotal
+            // 
+            txtTotal.CausesValidation = false;
+            txtTotal.Location = new Point(187, 216);
+            txtTotal.Name = "txtTotal";
+            txtTotal.ReadOnly = true;
+            txtTotal.Size = new Size(138, 27);
+            txtTotal.TabIndex = 3;
+            txtTotal.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 20);
+            label1.TabIndex = 0;
+            label1.Text = "&Subtotal:";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Discount Percent:";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(48, 167);
+            label3.Name = "label3";
+            label3.Size = new Size(127, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Discount Amount:";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(48, 223);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Total:";
+            label4.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.BackColor = SystemColors.ControlLight;
+            btnCalculate.Location = new Point(113, 262);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(94, 29);
+            btnCalculate.TabIndex = 2;
+            btnCalculate.Text = "&Calculate";
+            btnCalculate.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = SystemColors.ControlLight;
+            btnExit.Location = new Point(231, 262);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(94, 29);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AcceptButton = btnCalculate;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            CancelButton = btnExit;
+            ClientSize = new Size(356, 312);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculate);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtTotal);
+            Controls.Add(txtDiscountAmount);
+            Controls.Add(txtDiscountPercent);
+            Controls.Add(txtSubtotal);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Invoice Total";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtSubtotal;
+        private TextBox txtDiscountPercent;
+        private TextBox txtDiscountAmount;
+        private TextBox txtTotal;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button btnCalculate;
+        private Button btnExit;
+    }
+}
---
+++ frmInvoiceTotal.cs
@@ -0,0 +1,25 @@
+namespace Homework2
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
